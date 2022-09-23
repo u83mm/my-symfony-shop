07256e3860fe82d738b412410a69cde0da651be0
Mario Moreno <cursotecnoweb@gmail.com>
2022-09-23 23:48:23 +0200

    Added upload image option in Product Crud

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,9 +7,11 @@ use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/product')]
 class ProductController extends AbstractController
@@ -53,12 +55,32 @@ class ProductController extends AbstractController
 
     #[IsGranted('ROLE_ADMIN')]
     #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Product $product, ProductRepository $productRepository): Response
+    public function edit(Request $request, Product $product, ProductRepository $productRepository, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $img = $form->get('image')->getData();
+            if ($img) {
+                $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$img->guessExtension();
+
+                // Move the file to the directory where products are stored
+                try {
+                    $img->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    echo "The image can't be uploaded.";
+                }
+
+                // updates the 'image' property to store the IMG file name
+                // instead of its contents
+                $product->setImage($newFilename);
+            }
             $productRepository->add($product, true);
 
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
@@ -15,7 +17,17 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label'     => 'Upload Image',
+                'mapped'    => false,
+                'required'  => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',                        
+                        'mimeTypesMessage' => 'Please upload a valid IMG',
+                    ])
+                ]
+            ])
         ;
     }
 
